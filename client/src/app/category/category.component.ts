6531ca15db1299beeda26a7b6fa85b575c735330
Jared Avila <jared.c.avila@gmail.com>
2019-04-26 11:16:59 -0700

    updating more styles, added fucntionality to categories component

--- client/src/app/category/category.component.ts
+++ client/src/app/category/category.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  selector: "app-category",
+  templateUrl: "./category.component.html",
+  styleUrls: ["./category.component.scss"]
 })
 export class CategoryComponent implements OnInit {
+  @Input() category: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- client/src/app/featured-categories/featured-categories.component.ts
+++ client/src/app/featured-categories/featured-categories.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-featured-categories',
-  templateUrl: './featured-categories.component.html',
-  styleUrls: ['./featured-categories.component.scss']
+  selector: "app-featured-categories",
+  templateUrl: "./featured-categories.component.html",
+  styleUrls: ["./featured-categories.component.scss"]
 })
 export class FeaturedCategoriesComponent implements OnInit {
+  categories: Array<Object> = [
+    {
+      title: "Books/Magazines",
+      img: "../../assets/img/categories/books.jpg"
+    },
+    {
+      title: "Atrs/Crafts",
+      img: "../../assets/img/categories/arts.jpg"
+    },
+    {
+      title: "Tech/Gadets",
+      img: "../../assets/img/categories/tech.jpg"
+    },
+    {
+      title: "Food/Produce",
+      img: "../../assets/img/categories/food.jpg"
+    },
+    {
+      title: "Clothing/Apparel",
+      img: "../../assets/img/categories/apparel.jpg"
+    },
+    {
+      title: "Toys/Collectibles",
+      img: "../../assets/img/categories/toys.jpg"
+    },
+    {
+      title: "Home/Appliances",
+      img: "../../assets/img/categories/home.jpg"
+    },
+    {
+      title: "Outdoors/Garden",
+      img: "../../assets/img/categories/outdoor.jpg"
+    },
+    {
+      title: "Video Games",
+      img: "../../assets/img/categories/games.png"
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
